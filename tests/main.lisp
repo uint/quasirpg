4010074176113bd5ccae0cedc4bf9fe24e8de9e8
Tomek Kurcz <tomsandbox@gmail.com>
2017-09-04 13:57:18 +0200

    Randomize dice tests

--- tests/main.lisp
+++ tests/main.lisp
@@ -10,21 +10,30 @@
 (defun test-quasi ()
   (run! 'all-tests))
 
-(defun test-a-lot-of-dice ()
-  (every #'identity (loop for i from 1 to 100
-		       collecting (let ((result (quasirpg::roll-dice 2 10)))
-				    (and (>= result 2)
-					 (<= result 20))))))
+(defun gen-long-float (&key (max (1+ most-positive-long-float))
+			 (min (1- most-negative-long-float)))
+  (lambda () (+ min (random (1+ (- max min))))))
 
 (test dice-tests
   :description "Test the `roll-dice` function."
   (is (= 1 (quasirpg::roll-dice 1 1)))
   (is (= 3 (quasirpg::roll-dice 3 1)))
-  (is-true (test-a-lot-of-dice))
-  (is (= 3 (quasirpg::roll-dice 3.8 1)))
-  (is (= 3 (quasirpg::roll-dice 3 1.9)))
-  (signals simple-type-error (quasirpg::roll-dice 3 -1))
-  (signals simple-type-error (quasirpg::roll-dice 3 0))
-  (signals simple-type-error (quasirpg::roll-dice -1 2))
-  (signals simple-type-error (quasirpg::roll-dice 0 2))
-  (signals simple-type-error (quasirpg::roll-dice -1 1)))
+  (for-all ((n (gen-integer :min 1 :max 10))
+	    (sides (gen-integer :min 1 :max 10)))
+    "Test whether calls with random positive integers give results within expected bounds."
+    (let ((min n)
+	  (max (* n sides))
+	  (result (quasirpg::roll-dice n sides)))
+      (is (<= min result))
+      (is (>= max result))))
+  (for-all ((valid-float (gen-long-float :min 1 :max 100)))
+    "Test whether floats are rounded down."
+    (is (= (floor valid-float) (quasirpg::roll-dice valid-float 1)))
+    (is (>= (floor valid-float) (quasirpg::roll-dice 1 valid-float))))
+  (for-all ((invalid-int (gen-integer :max 0))
+	    (invalid-int2 (gen-integer :max 0))
+	    (valid-int (gen-integer :min 1)))
+    "Test whether non-positive numbers signal SIMPLE-TYPE-ERROR."
+    (signals simple-type-error (quasirpg::roll-dice valid-int invalid-int))
+    (signals simple-type-error (quasirpg::roll-dice invalid-int valid-int))
+    (signals simple-type-error (quasirpg::roll-dice invalid-int invalid-int2))))
